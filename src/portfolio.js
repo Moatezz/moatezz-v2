313585b9390b594aefb37ca5e65b72e434f608e2
Moatezz Chabane <moatezzChabane@gmail.com>
2022-01-25 01:40:41 +0100

    name and role

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,12 +2,12 @@ const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://rajshekhar26.github.io/cleanfolio',
   title: 'JS.',
-}
+};
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Moatezz Chabane',
+  role: 'Full Stack Web Apps Developer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
@@ -15,36 +15,33 @@ const about = {
     linkedin: 'https://linkedin.com',
     github: 'https://github.com',
   },
-}
+};
 
 const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
     name: 'Project 1',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    description: 'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
     stack: ['SASS', 'TypeScript', 'React'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
   {
     name: 'Project 2',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    description: 'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
     stack: ['SASS', 'TypeScript', 'React'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
   {
     name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    description: 'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
     stack: ['SASS', 'TypeScript', 'React'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
-]
+];
 
 const skills = [
   // skills can be added or removed
@@ -61,11 +58,11 @@ const skills = [
   'CI/CD',
   'Jest',
   'Enzyme',
-]
+];
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
   email: 'johnsmith@mail.com',
-}
+};
 
-export { header, about, projects, skills, contact }
+export { header, about, projects, skills, contact };
