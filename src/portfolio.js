62a497524a0e5b063a80264a1e1990988cd3dc72
Moatezz Chabane <moatezzChabane@gmail.com>
2022-01-25 01:52:27 +0100

    other info

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,11 +9,11 @@ const about = {
   name: 'Moatezz Chabane',
   role: 'Full Stack Web Apps Developer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'Professional full time freelancer  with full set of skills in front-end and back-end development in both Web and Mobile industries. I have a strong background in web development and have worked with many different languages and frameworks. I am a self-motivated and hard-working individual with a strong desire to learn new technologies and improve my skills. I am a team player and I am always willing to learn new things and improve my skills. I am a hard-working individual with a strong desire to learn new technologies and improve my skills. I am a team player and I am always willing to learn new things and improve my skills.',
+  resume: 'https://moatezz.com/resume.jpg',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'www.linkedin.com/in/moatezz',
+    github: 'https://github.com/moatezz',
   },
 };
 
